c7362768496090976b3b3f8340662e5df0f3a067
soumen Gupta <soumencse20@gmail.com>
2024-08-31 19:35:03 +0530

    cors issue fixed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(bodyparser.urlencoded({
     extended: true
   }));
   app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
   }));
